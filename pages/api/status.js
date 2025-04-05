424d48bfccd171385645635a1d16706f36ea2628
Aashna  Patel <aashnapatel@Vanshhs-Mac.local>
2025-04-06 04:47:21 +1000

    Wipe repo for fresh start

--- pages/api/status.js
+++
@@ -1,9 +0,0 @@
-export const config = { runtime: 'edge' };
-
-export default () => {
-  // In a real app you'd fetch from a database
-  return new Response(
-    JSON.stringify({ adminFund: 12500, capitalFund: 8750 }),
-    { headers: { 'Content-Type': 'application/json' } }
-  );
-};
